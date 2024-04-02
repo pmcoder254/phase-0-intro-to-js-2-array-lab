4888bf8dfe43bd3aa4fd2590a4af0c1f5b5b62fc
Paul Muchui <paul.muchungi@gmail.com>
2024-04-03 00:02:41 +0300

    makes the required changes however the slice part is not well understood. had to get help from the internet

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+let cats =["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat (){
+    cats.push("Ralph")
+}
+console.log = cats;
+
+function destructivelyPrependCat(){
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(){
+    return cats.concat("Broom");
+}
+
+function prependCat(){
+    return  ["Arnold"].concat(cats);
+}
+
+function removeLastCat(){
+    return cats.slice(0, -1)
+    
+}
+function removeFirstCat() {
+    return cats.slice(1)
+}
